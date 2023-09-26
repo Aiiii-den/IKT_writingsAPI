f93e730f8cb6a8106e6b66edbf836858066158ad
Aiiii-den <becker.work@web.de>
2023-09-26 09:59:36 +0200

    changed cors settings

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ const PORT = 3000;
 const cors = require('cors')
 
 app.use(express.json());
-app.use(cors({
-    origin: ['*']
-}));
-app.options('*', cors())
+app.use(cors());
 app.use((req, res, next) => {
     res.header({"Access-Control-Allow-Origin": "*"});
     next();
